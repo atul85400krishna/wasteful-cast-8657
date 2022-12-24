18c95b84844d64b016a3665760e6084b172cd221
atul85400krishna <atul85400krishna@gmail.com>
2022-12-24 15:22:59 +0530

    beaches_full_day_3

--- tripoto/src/components/beaches/Comprehensive.jsx
+++ tripoto/src/components/beaches/Comprehensive.jsx
@@ -1,121 +1,121 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
-// import Slider from "react-slick";
-// import { IoIosArrowDropleft, IoIosArrowDropright } from "react-icons/io";
-// import {
-//   WebseriesWrapper,
-//   WeseriesCard,
-// } from "../../styles/comprehensive.styled";
-// import {FaMapMarkerAlt} from "react-icons/fa";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import { IoIosArrowDropleft, IoIosArrowDropright } from "react-icons/io";
+import {
+  WebseriesWrapper,
+  WeseriesCard,
+} from "../../styles/comprehensive.styled";
+import {FaMapMarkerAlt} from "react-icons/fa";
 
-// const Comprehensive = () => {
-//   const [packag, setPackage] = useState([]);
-//   useEffect(() => {
-//     axios
-//       .get("https://tripoto-api-demo.herokuapp.com/comprehensive")
-//       .then((res) => {
-//         setPackage(res.data.ComprehensiveData);
-//       });
-//   }, []);
+const Comprehensive = () => {
+  const [packag, setPackage] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://tripoto-api-demo.herokuapp.com/comprehensive")
+      .then((res) => {
+        setPackage(res.data.ComprehensiveData);
+      });
+  }, []);
 
-//   function SampleNextArrow(props) {
-//     const { onClick } = props;
-//     return (
-//       <div
-//         style={{
-//           fontSize: "45px",
-//           color: "rgb(56,159,221)",
-//           backgroundColor: "transparent",
-//           position: "absolute",
-//           right: "-18px",
-//         }}
-//       >
-//         <IoIosArrowDropright onClick={onClick} />
-//       </div>
-//     );
-//   }
+  function SampleNextArrow(props) {
+    const { onClick } = props;
+    return (
+      <div
+        style={{
+          fontSize: "45px",
+          color: "rgb(56,159,221)",
+          backgroundColor: "transparent",
+          position: "absolute",
+          right: "-18px",
+        }}
+      >
+        <IoIosArrowDropright onClick={onClick} />
+      </div>
+    );
+  }
 
-//   function SamplePrevArrow(props) {
-//     const { onClick } = props;
+  function SamplePrevArrow(props) {
+    const { onClick } = props;
 
-//     return (
-//       <div
-//         style={{
-//           fontSize: "45px",
-//           color: "rgb(56,159,221)",
-//           backgroundColor: "transparent",
-//           position: "absolute",
-//           left: "-18px",
-//           zIndex: "999",
-//         }}
-//       >
-//         <IoIosArrowDropleft onClick={onClick} />
-//       </div>
-//     );
-//   }
+    return (
+      <div
+        style={{
+          fontSize: "45px",
+          color: "rgb(56,159,221)",
+          backgroundColor: "transparent",
+          position: "absolute",
+          left: "-18px",
+          zIndex: "999",
+        }}
+      >
+        <IoIosArrowDropleft onClick={onClick} />
+      </div>
+    );
+  }
 
-//   let settings = {
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 3,
-//     slidesToScroll: 3,
-//     nextArrow: <SampleNextArrow />,
-//     prevArrow: <SamplePrevArrow />,
-//     responsive: [
-//       {
-//         breakpoint: 1024,
-//         settings: {
-//           slidesToShow: 3,
-//           slidesToScroll: 3,
-//           infinite: true,
-//           dots: true
-//         }
-//       },
-//       {
-//         breakpoint: 600,
-//         settings: {
-//           slidesToShow: 2,
-//           slidesToScroll: 2,
-//           initialSlide: 2
-//         }
-//       },
-//       {
-//         breakpoint: 480,
-//         settings: {
-//           slidesToShow: 1,
-//           slidesToScroll: 1
-//         }
-//       }
-//     ]
-//   };
-//   return (
-//     <WebseriesWrapper>
-//       <div>
-//         {/* <h1>Explore Our Comprehensive Guides for More Details</h1> */}
-//       </div>
-//       <Slider
-//         {...settings}
-//         style={{
-//           margin: "auto",
-//           alignItems: "center",
-//           width: "100%",
-//           display: "flex",
-//         }}
-//       >
-//         {packag &&
-//           packag.map((ele) => (
-//             <WeseriesCard key={ele._id}>
-//               <img src={ele.image}></img>
+  let settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+  return (
+    <WebseriesWrapper>
+      <div>
+        {/* <h1>Explore Our Comprehensive Guides for More Details</h1> */}
+      </div>
+      <Slider
+        {...settings}
+        style={{
+          margin: "auto",
+          alignItems: "center",
+          width: "100%",
+          display: "flex",
+        }}
+      >
+        {packag &&
+          packag.map((ele) => (
+            <WeseriesCard key={ele._id}>
+              <img src={ele.image}></img>
          
-//               <h3>{ele.title}</h3>
+              <h3>{ele.title}</h3>
             
-//             </WeseriesCard>
-//           ))}
-//       </Slider>
-//     </WebseriesWrapper>
-//   );
-// }
+            </WeseriesCard>
+          ))}
+      </Slider>
+    </WebseriesWrapper>
+  );
+}
 
-// export default Comprehensive
+export default Comprehensive
